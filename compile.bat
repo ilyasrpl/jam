e735acd31f9b3602e36c1e52eb826156ba424f29
ilyasrpl <ilyasrpl@gmail.com>
2026-06-04 10:56:07 +0700

    fix

--- compile.bat
+++ compile.bat
@@ -2,8 +2,8 @@
 REM Set the path to csc.exe as a variable
 set CSC_PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe
 
-REM Compile jam.cs using the variable
-%CSC_PATH% jam.cs
+REM Compile jam.cs as a GUI application (winexe)
+%CSC_PATH% /target:winexe /out:jam.exe jam.cs
 
 REM Check if compilation was successful
 if %errorlevel% equ 0 (
